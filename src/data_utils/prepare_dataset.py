eaa6e46190f9ae1394381b9a748f989ea13b1545
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-11-03 22:58:58 +0530

    push from local

--- src/data_utils/prepare_dataset.py
+++ src/data_utils/prepare_dataset.py
@@ -60,12 +60,10 @@ def prepare_dataset(config: AttributeHashmap, transforms_list = [None, None, Non
                        return_format='one_pair') #transforms=transforms_train, transforms_aug=transforms_aug
     val_set = Subset(main_dataset=dataset,
                      subset_indices=val_indices,
-                     return_format='all_pairs',
-                     transforms=transforms_val)
+                     return_format='all_pairs')  #transforms=transforms_val
     test_set = Subset(main_dataset=dataset,
                       subset_indices=test_indices,
-                      return_format='all_pairs',
-                      transforms=transforms_test)
+                      return_format='all_pairs')  #transforms=transforms_test
 
     min_sample_per_epoch = 5
     if 'max_training_samples' in config.keys():
