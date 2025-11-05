a5fb278483bf36e9416f6b489d2ebca7b16a71bd
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-11-06 00:48:08 +0530

    push from local

---
+++ src/train.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=imageflownet        # Job name
+#SBATCH --output=logs/imageflownet_%j.out
+#SBATCH --error=logs/imageflownet_%j.out
+#SBATCH --time=48:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=30G
+#SBATCH --partition=gpu_l40
+#SBATCH --gpus=1
+
+#export CUDA_VISIBLE_DEVICES='0'
+#export PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True'
+#export NUMEXPR_MAX_THREADS=64
+
+source /home/rs1/21CS92R01/miniconda3/bin/activate crystal-llm
+
+
+python train_2pt_all.py --model ImageFlowNetODE --random-seed 1 --dataset-name synthetic --max-epochs 100
